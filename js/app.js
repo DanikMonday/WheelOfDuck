e06dac6d9647cbd47f2011e1ea0310e86fec3df0
Miguel Roa <miguel_roa150@outlook.es>
2022-11-28 11:18:40 -0500

    Se agrega ventana modal con informacion de los eliminados

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+// variables abrir/cerrar de la ventana modal, eventlistener.
+const open = document.getElementById('open');//varible open, trae al elemento por su id open
+const modal_container = document.getElementById('modal_container');//varible hace referencia al id modal_container y la invoca
+const close = document.getElementById('close');// variable close trae el elemento por su id close
+//Evento listener click, con la funcion para la variable añadiendo la clase show
+open.addEventListener('click', () => {
+  modal_container.classList.add('show');  
+  
+});
+//Evento listener click, con la funcion para la variable removiendo la clase show
+close.addEventListener('click', () => {
+  modal_container.classList.remove('show');
+});
+
+
+
+
 //* INDEX HTML - FONDO EFECTO PANORÁMICA 360°
 
 // Variable para seleccionar el elemento que contendrá el fondo
