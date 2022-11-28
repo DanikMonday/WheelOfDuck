612b02ce68596453735bdd01220bc25542970b69
Derick Saaa <dmsaao@unal.edu.co>
2022-11-28 11:49:01 -0500

    cambio de funcio en players

--- js/players.js
+++ js/players.js
@@ -1,14 +1,12 @@
 const form = document.getElementById('submit_Player');
 const list = document. querySelector('.lista');
 
-form.addEventListener('submit', (event)=> {
+form.addEventListener('submit', funcion )
+function funcion (event){
     event.preventDefault()
-    const inputValue = event.target.player_name.value // hace referencia al elemento html que tiene el addeventlistener 
+    const inputValue = event.target.player_name.value
     const newitem = document.createElement('li')
     newitem.appendChild(document.createTextNode(inputValue))
     list.appendChild(newitem)
-    event.target.player_name.value = ''
-})
-
-
-
+    event.target.player_name.value=''
+}
