de645f0e9240e292bde5ec5a4fcb8463ac540e08
Derick Saaa <dmsaao@unal.edu.co>
2022-11-28 16:17:46 -0500

    matriz de nombre aleatoria

--- js/players.js
+++ js/players.js
@@ -1,5 +1,7 @@
 const form = document.getElementById('submit_Player');
 const list = document. querySelector('.lista');
+const start = document.querySelector('#start')
+const arrayNombres = [];
 
 form.addEventListener('submit', funcion )
 function funcion (event){
@@ -9,4 +11,16 @@ function funcion (event){
     newitem.appendChild(document.createTextNode(inputValue))
     list.appendChild(newitem)
     event.target.player_name.value=''
+    //agregando la matriz de nombres 
+    arrayNombres.push(inputValue)
+    console.log(arrayNombres)
 }
+
+start.addEventListener('click', (e)=>{
+    e.preventDefault()
+    arrayNombres.sort(()=>Math.random()-0.5)
+    console.log(arrayNombres)
+    // const aleat = Math.floor(Math.random()*arrayNombres.length)
+    // console.log(arrayNombres[aleat])
+})
+
