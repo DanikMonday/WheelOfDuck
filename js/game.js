30b58e1685dc16481d72b118a6aeaf636f6cbacf
Derick Saaa <dmsaao@unal.edu.co>
2022-12-05 17:31:50 -0500

    lo que hizo miguel

--- js/game.js
+++ js/game.js
@@ -2,9 +2,15 @@ const container = document.querySelector('.carrusel-items')
 const circulo = document.querySelector('.circulo1')
 const dianas = document.getElementsByClassName('circulo1')
 const puntos = document.querySelector('#contador')
+
+const open = document.getElementById('open');//varible open, trae al elemento por su id open
+const modal_container = document.getElementById('modal_container');//varible hace referencia al id modal_container y la invoca
+const close = document.getElementById('close');// variable close trae el elemento por su id close
+
 let score = 0 
 
 const cursor = document.querySelector(".cursor");
+const patoeliminado = document.querySelector('#patos')
 
 function obtenerArray() {
     arrayN = JSON.parse(localStorage.getItem('arrayNom'))
@@ -22,13 +28,19 @@ for (let x = 0; x < (dianas.length); x++) {
     dianas[x].addEventListener('click',(e)=>{
         const eliminado = arrayN.splice((0),1)
         score+=1
+        modal_container.classList.add('show'); 
         // const eliminado = arrayN.splice((Math.floor( Math.random()*(arrayN.length))),1)
         console.log(arrayN)
         console.log(eliminado)
         container.removeChild(dianas[0])
         puntos.innerHTML = 'Puntos: ' + String(score)
+        patoeliminado.innerHTML = 'Pato Eliminado: ' + String(eliminado);
     })
 }
+
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+});
 
 function movimientoCursor() {
     window.addEventListener("mousemove", (e) => {
