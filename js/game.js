e6bc92142b9730c965f4f74242acac70f19b0e4a
Derick Saaa <dmsaao@unal.edu.co>
2022-12-05 16:14:17 -0500

    solucionado problema del ultimo pato

--- js/game.js
+++ js/game.js
@@ -8,7 +8,7 @@ const cursor = document.querySelector(".cursor");
 
 function obtenerArray() {
     arrayN = JSON.parse(localStorage.getItem('arrayNom'))
-    console.log(arrayN.length)
+    // console.log(arrayN.length)
 }
 
 obtenerArray()
@@ -20,12 +20,12 @@ for (let x = 0; x < (arrayN.length-1); x++) {
 
 for (let x = 0; x < (dianas.length); x++) {
     dianas[x].addEventListener('click',(e)=>{
-        const eliminado = arrayN.splice((x),1)
+        const eliminado = arrayN.splice((0),1)
         score+=1
         // const eliminado = arrayN.splice((Math.floor( Math.random()*(arrayN.length))),1)
         console.log(arrayN)
-        // console.log(eliminado)
-        container.removeChild(dianas[x])
+        console.log(eliminado)
+        container.removeChild(dianas[0])
         puntos.innerHTML = 'Puntos: ' + String(score)
     })
 }
