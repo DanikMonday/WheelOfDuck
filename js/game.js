938992fa4f1e5e187e283bfaa5198bb823d277c0
Derick Saaa <dmsaao@unal.edu.co>
2022-12-05 12:10:29 -0500

    contador listo

--- js/game.js
+++ js/game.js
@@ -1,6 +1,8 @@
 const container = document.querySelector('.carrusel-items')
 const circulo = document.querySelector('.circulo1')
 const dianas = document.getElementsByClassName('circulo1')
+const puntos = document.querySelector('#contador')
+let score = 0 
 
 function obtenerArray() {
     arrayN = JSON.parse(localStorage.getItem('arrayNom'))
@@ -17,10 +19,12 @@ for (let x = 0; x < (arrayN.length-1); x++) {
 for (let x = 0; x < (dianas.length); x++) {
     dianas[x].addEventListener('click',(e)=>{
         const eliminado = arrayN.splice((x),1)
+        score+=1
         // const eliminado = arrayN.splice((Math.floor( Math.random()*(arrayN.length))),1)
         console.log(arrayN)
         // console.log(eliminado)
         container.removeChild(dianas[x])
+        puntos.innerHTML = 'Puntos: ' + String(score)
     })
 }
 
