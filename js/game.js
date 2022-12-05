023ac8bb410a1c8bb666ccd1f044786913acc776
DanikMonday <daacostaf@unal.edu.co>
2022-12-05 15:44:04 -0500

    gameover

---
+++ js/game.js
@@ -0,0 +1,38 @@
+const container = document.querySelector('.carrusel-items')
+const circulo = document.querySelector('.circulo1')
+const dianas = document.getElementsByClassName('circulo1')
+
+const cursor = document.querySelector(".cursor");
+
+function obtenerArray() {
+    arrayN = JSON.parse(localStorage.getItem('arrayNom'))
+    console.log(arrayN.length)
+}
+
+obtenerArray()
+
+
+for (let x = 0; x < (arrayN.length-1); x++) {
+    container.appendChild(circulo.cloneNode(true))
+}
+
+
+for (let x = 0; x < (dianas.length); x++) {
+    dianas[x].addEventListener('click',(e)=>{
+        const eliminado = arrayN.splice((x),1)
+        // const eliminado = arrayN.splice((Math.floor( Math.random()*(arrayN.length))),1)
+        console.log(arrayN)
+        // console.log(eliminado)
+        container.removeChild(dianas[x])
+    })
+}
+
+function movimientoCursor() {
+  window.addEventListener("mousemove", (e) => {
+    cursor.style.top = e.pageY + "px";
+    cursor.style.left = e.pageX + "px";
+  });
+
+}
+
+movimientoCursor();
